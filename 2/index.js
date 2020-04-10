cdf501abd15b94564b361cba7b7f4c3af8de3dac
Eddie Strada <dudu.gemmal@gmail.com>
2020-04-10 16:40:49 +0900

    saving latest progress on making the form functional

--- 2/index.js
+++ 2/index.js
@@ -61,6 +61,20 @@ setTimeout(function() {
 }, 1650);
 
 $(document).ready(function() {
+  $("#bt1").click(function() {
+    $("#fr1").attr(
+      "action",
+      "mailto:dudu.gemmal@gmail.com?subject=" +
+        $("#tb1").val() +
+        $("#tb2").val() +
+        "&body=" +
+        $("#tb3").val()
+    );
+    $("#fr1").submit();
+  });
+});
+
+$(document).ready(function() {
   $("a").on("click", function(event) {
     if (this.hash !== "") {
       event.preventDefault();
